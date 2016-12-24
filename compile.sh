ac05138d66ee384cb84909ab1d24c4706cb939fd
p-andelfinger <philipp.andelfinger@kit.edu>
2016-12-24 11:45:14 +0100

    removed architecture flag

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-/usr/local/cuda/bin/nvcc -arch=compute_50 -O3 -Xcompiler -Wno-write-strings -I. cuda_gl_interop.cu -o cuda_gl_interop -lcudart -I/usr/include/sfml -lsfml-graphics -lsfml-window -lsfml-system
+/usr/local/cuda/bin/nvcc -O3 -Xcompiler -Wno-write-strings -I. cuda_gl_interop.cu -o cuda_gl_interop -lcudart -I/usr/include/sfml -lsfml-graphics -lsfml-window -lsfml-system
